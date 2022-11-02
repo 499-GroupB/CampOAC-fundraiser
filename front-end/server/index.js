03d79ed64858c17e2557982b6317a03c2719dfba
Baillie Stang <baillie.stang@gmail.com>
2022-11-02 12:59:19 -0700

    Added backend
    Added backend and changed the start script to build production elements

---
+++ front-end/server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// configure
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Start express and listen on port 5000
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+});
