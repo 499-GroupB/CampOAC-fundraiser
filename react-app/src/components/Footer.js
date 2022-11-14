02edf3c2f35d6575554590b2161bc1e14c4f897e
SaiRohithEnumala8373 <esairohith@gmail.com>
2022-11-14 12:10:46 -0800

    Added the hyperlinks and about information

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -6,9 +6,9 @@ import "../css/Style.css";
 const Footer = () => {
     return (
         <footer className="footer-class">
-            <FontAwesomeIcon icon={faFacebook} size="lg"></FontAwesomeIcon>
+            <a href="https://www.facebook.com/campoac/"><FontAwesomeIcon icon={faFacebook} size="lg"></FontAwesomeIcon></a>
             <FontAwesomeIcon icon={faTwitter} size="lg"></FontAwesomeIcon>
-            <FontAwesomeIcon icon={faInstagram} size="lg"></FontAwesomeIcon>
+            <a href = "https://www.instagram.com/camp_oac/"><FontAwesomeIcon icon={faInstagram} size="lg"></FontAwesomeIcon></a>
             <p id="footerText">Made by CampOAC group b</p>
             <p id="footerText">All rights reserved CampOAC x Rotary Club of Kelowna</p>
         </footer>
--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -4,7 +4,21 @@ const About = () => {
     return (
         <>
             <h1>About</h1>
-            <p>Fundraising information goes here and all that</p>
+            
+            
+            <p>We are currently doing the following fundraisers to raise
+            money for the new craft hut and to support camper bursaries. 
+            We believe that all youth in our community should have the 
+            opportunity to experience summer camp! </p>
+
+            <h3> Firewood Fundraiser</h3>
+
+            <p>
+            We have partnered with the Rotary Club of Kelowna
+            Ogopogo to sell firewood. Stay tuned for more information
+            in the spring.
+
+            </p>
         </>
     );
   };
