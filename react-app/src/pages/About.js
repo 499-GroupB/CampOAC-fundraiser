0a05f59321368a5a0c282d8cc7d8ddd0293cf6af
boulongadong <77290153+boulongadong@users.noreply.github.com>
2022-11-18 12:33:23 -0800

    removed calls to invoicing

--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -1,28 +1,28 @@
 // About page
 
 const About = () => {
-    return (
-        <>
-            <h1>rotary club of kelowna ogopogo</h1>
-            
-            <p>
-            We accomplish service projects in the local community
-            and internationally through the fellowship and efforts
-            of our club members.  More information is available here.
-            </p>
+  return (
+      <>
+          <h1>rotary club of kelowna ogopogo</h1>
+          
+          <p>
+          We accomplish service projects in the local community
+          and internationally through the fellowship and efforts
+          of our club members.  More information is available here.
+          </p>
 
-            
-           
+          
+         
 
-            <h3>Camp OAC</h3>
+          <h3>Camp OAC</h3>
 
-            <p>
-            Camp OAC is a children's summer camp located just north of Kelowna on Okanagan lake.
-              More information is available here.
+          <p>
+          Camp OAC is a children's summer camp located just north of Kelowna on Okanagan lake.
+            More information is available here.
 
-            </p>
-        </>
-    );
-  };
-  
-  export default About;
+          </p>
+      </>
+  );
+};
+
+export default About;
