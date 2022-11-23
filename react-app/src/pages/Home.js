c18d5b2c142f12c63799b47f9010bd09f0f9e48a
paulrang <77507367+paulrang@users.noreply.github.com>
2022-11-23 11:25:36 -0800

    Fixed images for most window sizes

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -6,9 +6,15 @@ import '../css/Style.css';
 
 function fixSize() { //Add this on load at some point
   document.querySelectorAll('.column').forEach(column => {
-    column.style.height = window.innerWidth/7.8+"px";
-    column.style.paddingLeft = window.innerWidth/150+"px";
+    column.style.paddingLeft = "1.2%";
   });
+  document.getElementById("image1").style.width = "10.3%";
+  document.getElementById("image2").style.width = "10.3%";
+  document.getElementById("image3").style.width = "17.55%";
+  document.getElementById("image4").style.width = "17.55%";
+  document.getElementById("image5").style.width = "22.3%";
+  document.getElementById("image6").style.width = "10.3%";
+  document.getElementById("image7").style.width = "10.3%";
 }
 window.addEventListener('resize', function(event){fixSize();},true);
 const Home = () => {
@@ -17,25 +23,25 @@ const Home = () => {
 
 
 <div class = "row">
-  <div class = "column">
+  <div class = "column" id="image1">
     <img src = "firewoodPics/IMG_8859.jpeg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image2">
     <img src = "firewoodPics/IMG_9118.jpeg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image3">
     <img src = "firewoodPics/Firewood pic 2 - small.jpg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image4">
     <img src = "firewoodPics/Firewood Team.jpg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image5">
     <img src = "firewoodPics/FirewoodBags.jpg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image6">
     <img src = "firewoodPics/IMG_8688 2.jpeg" width="100%;" height="100%"/>
   </div>
-  <div class = "column">
+  <div class = "column" id="image7">
     <img src = "firewoodPics/IMG_8690.jpeg" width="100%;" height="100%"/>
   </div>
 </div>
