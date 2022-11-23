9fd3271f6ee27ebf08caaf44c1b16f8b132cf3ea
SaiRohithEnumala8373 <esairohith@gmail.com>
2022-11-23 12:53:59 -0800

    same images on about page

--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -1,8 +1,44 @@
 // About page
 
+
+const styleSma = {
+  width: "10.3%"
+}
+const styleMed = {
+  width: "17.55%"
+}
+const styleLar = {
+  width: "22.3%"
+}
+
 const About = () => {
     return (
         <>
+
+
+<div class = "row">
+  <div class = "column" id="image1" style={styleSma}>
+    <img src = "firewoodPics/IMG_8859.jpeg" width="100%"/>
+  </div>
+  <div class = "column" id="image2" style={styleSma}>
+    <img src = "firewoodPics/IMG_9118.jpeg" width="100%"/>
+  </div>
+  <div class = "column" id="image3" style={styleMed}>
+    <img src = "firewoodPics/Firewood pic 2 - small.jpg" width="100%"/>
+  </div>
+  <div class = "column" id="image4" style={styleMed}>
+    <img src = "firewoodPics/Firewood Team.jpg" width="100%"/>
+  </div>
+  <div class = "column" id="image5" style={styleLar}>
+    <img src = "firewoodPics/FirewoodBags.jpg" width="100%"/>
+  </div>
+  <div class = "column" id="image6" style={styleSma}>
+    <img src = "firewoodPics/IMG_8688 2.jpeg" width="100%"/>
+  </div>
+  <div class = "column" id="image7" style={styleSma}>
+    <img src = "firewoodPics/IMG_8690.jpeg" width="100%"/>
+  </div>
+</div>
 
             <h1>rotary club of kelowna ogopogo</h1>
             
