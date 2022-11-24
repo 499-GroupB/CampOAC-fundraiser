c6b7996a21dc6b2a6a0ef3af7fc4fb7d967340ee
SaiRohithEnumala8373 <esairohith@gmail.com>
2022-11-24 15:56:19 -0800

    Logo Added

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -6,11 +6,21 @@ import "../css/Style.css";
 const Footer = () => {
     return (
         <footer className="footer-class">
+
+            <div class = "text">
             <a href="https://www.facebook.com/campoac/"><FontAwesomeIcon icon={faFacebook} size="lg"></FontAwesomeIcon></a>
             &nbsp;&nbsp;
             <a href = "https://www.instagram.com/camp_oac/"><FontAwesomeIcon icon={faInstagram} size="lg"></FontAwesomeIcon></a>
             <p id="footerText">Made by Camp OAC Group-B</p>
             <p id="footerText">All rights reserved CampOAC x Rotary Club of Kelowna</p>
+            </div>
+    <div class="logo">
+        <img src="https://media.discordapp.net/attachments/931270620547784754/1045467416894902332/Camp-OAC-Logo-Primary.png" width="120px" height="120px" ></img>
+        &nbsp;&nbsp; &nbsp;&nbsp;
+        <img src="https://media.discordapp.net/attachments/931270620547784754/1045468871781523456/officialclublogo_rotary.png" width="150px" height="120px" ></img>
+            
+    </div>
+
             
         </footer>
     );
