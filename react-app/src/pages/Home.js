8d5223fcdb225e082df31bf17f95bdd54f4835c8
paulrang <77507367+paulrang@users.noreply.github.com>
2022-12-02 14:28:56 -0800

    Text changes on the home and about pages

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -41,18 +41,11 @@ const Home = () => {
     <img src = "firewoodPics/IMG_8690.jpeg" width="100%"/>
   </div>
 </div>
-
-
-             
-            <h1>welcome to the camp oac firewood fundraiser</h1>
-            <p>Rotary Club of Kelowna Ogopogo
-                <br/>We accomplish service projects in the local community and internationally through the fellowship and efforts of our club members.  More information is available here.
-                <br/><br/>Camp OAC
-                <br/>Camp OAC is a children's summer camp located just north of Kelowna on Okanagan lake.  More information is available here.</p>
-            <br/>
-            <Link to="/order"><button>Place Order</button></Link>
-
-            </>
+<h1>welcome to the camp oac firewood fundraiser</h1>
+<p>Camp OAC is a childrens summer camp located just north of Kelowna on the beautiful Okanagan lake! Since 1950, Camp OAC has had thousands of campers attend their summer camps. In the offseason, the amazing property is available for rentals to host family reunions, get togethers, weddings and even school group rentals. Camp OAC is a place that everyone can enjoy.
+<br/><br/>On this website you can purchase firewood to support Camp OAC!</p>
+<Link to="/order"><button>Place Order</button></Link>
+        </>
 
 
           
