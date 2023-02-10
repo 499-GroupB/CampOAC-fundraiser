7b0f14538dd775d8b107e6fffaeef0be8ec52fde
boulongadong <77290153+boulongadong@users.noreply.github.com>
2023-02-10 11:14:48 -0800

    added sms invoice body

--- react-app/server/models/location.js
+++ react-app/server/models/location.js
@@ -7,11 +7,6 @@ const Schema = mongoose.Schema;
 const LocationSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   stock: { type: Number, required: true, min: [0, 'Too few wood'], max: 1000 },
-  address: String,
-  admin: {
-    name: String,
-    phone: String,
-  },
 });
 
 // Export model
