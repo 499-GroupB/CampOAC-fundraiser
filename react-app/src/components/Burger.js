cbc20393411c3711f0c29076728f150eba53154d
paulrang <77507367+paulrang@users.noreply.github.com>
2023-02-15 10:20:06 -0800

    Moved some burger menu css

--- react-app/src/components/Burger.js
+++ react-app/src/components/Burger.js
@@ -1,49 +1,18 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import RightNav from './RightNav';
-
-const StyledBurger = styled.div`
-  width: 5rem;
-  height: 2rem;
-  top: 15px;
-  right: 25px;
-  z-index: 20;
-  display: none;
-  @media (max-width: 837px) {
-    display: flex;
-    justify-content: space-around;
-    flex-flow: column nowrap;
-  }
-  div {
-    width: 2rem;
-    height: 0.25rem;
-    background-color: ${({ open }) => open ? '#ccc' : '#fff'};
-    border-radius: 10px;
-    transform-origin: 1px;
-    transition: all 0.3s linear;
-    &:nth-child(1) {
-      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
-    }
-    &:nth-child(2) {
-      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
-      opacity: ${({ open }) => open ? 0 : 1};
-    }
-    &:nth-child(3) {
-      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
-    }
-  }
-`;
+import '../css/Nav.css';
 
 const Burger = () => {
   const [open, setOpen] = useState(false)
 
   return (
     <>
-      <StyledBurger open={open} onClick={() => setOpen(!open)}>
-        <div />
-        <div />
-        <div />
-      </StyledBurger>
+      <input type="checkbox" id="burgControl"/>
+      <label id="burg" for="burgControl" open={open} onClick={() => setOpen(!open)}>
+        <div class="split" id="split1"/>
+        <div class="split" id="split2"/>
+        <div class="split" id="split3"/>
+      </label>
       <RightNav open={open} />
     </>
   )
