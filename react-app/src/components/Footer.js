8a30b57844e9c1eb56d7b931234a45dccd41fd16
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-03-09 12:33:03 -0800

    some changes

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -11,12 +11,16 @@ const Footer = () => {
             </div>
 
             <div>
+                <div class = "visit">
                 <span class="footerText">Visit us:&nbsp;</span><a class="footericons" href="https://www.facebook.com/campoac/"><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></a>
                 &nbsp;&nbsp;
                 <a class="footericons" href="https://www.instagram.com/camp_oac/"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a>
-                <br/>
+                </div>
+               
                 {/* <span class="footerText">Share:&nbsp;</span> */}
+                <div>
                 <span class="footericons fb-share-button" data-href="http://capstone.bstang.ca:3000/" data-layout="" data-size=""><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fcapstone.bstang.ca%3A3000%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>&nbsp;Share</a></span>
+                </div>
                 <p id="footerText">Made by Camp OAC Group-B</p>
                 <p id="footerText">All rights reserved CampOAC &times; Rotary Club of Kelowna</p>
             </div>
