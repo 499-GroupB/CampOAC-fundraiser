bdd99743998db672b013a66684521d8309fc25a3
paulrang <77507367+paulrang@users.noreply.github.com>
2023-03-24 10:49:23 -0700

    Moved images to different file and added button to change image

--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -1,17 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import Images from '../components/Images';
 
 // About page
-
-function modd(srcc, altt) {
-  document.getElementById("myModal").style.display = "block";
-  document.getElementById("img01").src = srcc;
-  document.getElementById("caption").innerHTML = altt;
-}
-
-function close() {
-  document.getElementById("myModal").style.display = "none";
-}
 
 const About = () => {
 
@@ -78,42 +69,7 @@ const About = () => {
 
 return (
   <>
-    <div class="row">
-      <div class="column imgSma">
-        <img id="img1" src="firewoodPics/IMG_8859.jpeg" width="100%" alt="It takes a team."
-          onClick={() => modd(document.getElementById("img1").src, document.getElementById("img1").alt)} />
-      </div>
-      <div class="column imgSma">
-        <img id="img2" src="firewoodPics/IMG_9118.jpeg" width="100%" alt="Proud of our firewood."
-          onClick={() => modd(document.getElementById("img2").src, document.getElementById("img2").alt)} />
-      </div>
-      <div class="column imgMed">
-        <img id="img3" src="firewoodPics/Firewood_pic_2_-_small.jpg" width="100%" alt="Heavy machinery moving wood."
-          onClick={() => modd(document.getElementById("img3").src, document.getElementById("img3").alt)} />
-      </div>
-      <div class="column imgMed">
-        <img id="img4" src="firewoodPics/Firewood_Team.jpg" width="100%" alt="The team with the wood splitter."
-          onClick={() => modd(document.getElementById("img4").src, document.getElementById("img4").alt)} />
-      </div>
-      <div class="column imgLar">
-        <img id="img5" src="firewoodPics/FirewoodBags.jpg" width="100%" alt="Some bags of firewood we sold in a previous fundraiser."
-          onClick={() => modd(document.getElementById("img5").src, document.getElementById("img5").alt)} />
-      </div>
-      <div class="column imgSma">
-        <img id="img6" src="firewoodPics/IMG_8688_2.jpeg" width="100%" alt="Cutting the firewood."
-          onClick={() => modd(document.getElementById("img6").src, document.getElementById("img6").alt)} />
-      </div>
-      <div class="column imgSma">
-        <img id="img7" src="firewoodPics/IMG_8690.jpeg" width="100%" alt="Moving some wood."
-          onClick={() => modd(document.getElementById("img7").src, document.getElementById("img7").alt)} />
-      </div>
-    </div>
-    <div id="myModal" class="modal">
-      <span class="modalbackground" onClick={() => close()}/>
-      <span class="close" onClick={() => close()}>&times;</span>
-      <img class="modal-content" id="img01"/>
-      <div id="caption"></div>
-    </div>
+    <Images/>
     <br />
     <h2 class="bearhug">rotary club of kelowna ogopogo</h2>
     <p>We accomplish service projects in the local community and internationally through the fellowship and efforts of our club members. <a class="bodylink" href="https://my.rotary.org/en/">More information is available here.</a></p>
--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -3,58 +3,12 @@
 
 import { Link } from 'react-router-dom';
 import '../css/Style.css';
-
-function modd(srcc, altt) {
-  document.getElementById("myModal").style.display = "block";
-  document.getElementById("img01").src = srcc;
-  document.getElementById("caption").innerHTML = altt;
-}
-
-function close() {
-  document.getElementById("myModal").style.display = "none";
-}
+import Images from '../components/Images';
 
 const Home = () => {
   return (
     <>
-      <div class="row">
-        <div class="column imgSma">
-          <img id="img1" src="firewoodPics/IMG_8859.jpeg" width="100%" alt="It takes a team."
-            onClick={() => modd(document.getElementById("img1").src, document.getElementById("img1").alt)} />
-        </div>
-        <div class="column imgSma">
-          <img id="img2" src="firewoodPics/IMG_9118.jpeg" width="100%" alt="Proud of our firewood."
-            onClick={() => modd(document.getElementById("img2").src, document.getElementById("img2").alt)} />
-        </div>
-        <div class="column imgMed">
-          <img id="img3" src="firewoodPics/Firewood_pic_2_-_small.jpg" width="100%" alt="Heavy machinery moving wood."
-            onClick={() => modd(document.getElementById("img3").src, document.getElementById("img3").alt)} />
-        </div>
-        <div class="column imgMed">
-          <img id="img4" src="firewoodPics/Firewood_Team.jpg" width="100%" alt="The team with the wood splitter."
-            onClick={() => modd(document.getElementById("img4").src, document.getElementById("img4").alt)} />
-        </div>
-        <div class="column imgLar">
-          <img id="img5" src="firewoodPics/FirewoodBags.jpg" width="100%" alt="Some bags of firewood we sold in a previous fundraiser."
-            onClick={() => modd(document.getElementById("img5").src, document.getElementById("img5").alt)} />
-        </div>
-        <div class="column imgSma">
-          <img id="img6" src="firewoodPics/IMG_8688_2.jpeg" width="100%" alt="Cutting the firewood."
-            onClick={() => modd(document.getElementById("img6").src, document.getElementById("img6").alt)} />
-        </div>
-        <div class="column imgSma">
-          <img id="img7" src="firewoodPics/IMG_8690.jpeg" width="100%" alt="Moving some wood."
-            onClick={() => modd(document.getElementById("img7").src, document.getElementById("img7").alt)} />
-        </div>
-      </div>
-
-      <div id="myModal" class="modal">
-        <span class="modalbackground" onClick={() => close()}/>
-        <span class="close" onClick={() => close()}>&times;</span>
-        <img class="modal-content" id="img01"/>
-        <div id="caption"></div>
-      </div>
-
+      <Images/>
       <h1>welcome to the camp oac <b>&times;</b> rotary club ogopogo firewood fundraiser</h1>
       <Link id="orderbtn" to="/order"><button class="rounded">Place Order</button></Link>
       <p>Camp OAC is a childrens summer camp located just north of Kelowna on the beautiful Okanagan lake! Since 1950, Camp OAC has had thousands of campers attend their summer camps. In the offseason, the amazing property is available for rentals to host family reunions, get togethers, weddings and even school group rentals. Camp OAC is a place that everyone can enjoy.
