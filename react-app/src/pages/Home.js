d0c8b9f4dce36ffdcd36dcbc765785db2f9a4852
boulongadong <77290153+boulongadong@users.noreply.github.com>
2023-04-10 15:49:18 -0700

    low low prices

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -9,11 +9,11 @@ const Home = () => {
   return (
     <>
       <Images/>
-      <h1>welcome to the camp oac <b>&times;</b> rotary club ogopogo firewood fundraiser</h1>
-      <Link id="orderbtn" to="/order"><button class="rounded">Place Order</button></Link>
-      <p>Camp OAC is a childrens summer camp located just north of Kelowna on the beautiful Okanagan lake! Since 1950, Camp OAC has had thousands of campers attend their summer camps. In the offseason, the amazing property is available for rentals to host family reunions, get togethers, weddings and even school group rentals. Camp OAC is a place that everyone can enjoy.
-        <br/><br/>The Rotary Club of Kelowna Ogopogo, chartered in 1993, is a group of 52 men and women, and is one of eight Rotary clubs in our area. In addition to providing support to our own community, our club has an active focus on programs involving youth. As well, both directly and through our involvement with Rotary International's Foundation, we assist with major international projects including disaster relief, fresh water development projects, and health programs such as PolioPlus.
-        <br/><br/>On this website you can purchase firewood to support us!</p>
+      <h2 class='bearhug'>welcome to the camp oac <b>&times;</b> rotary club ogopogo firewood fundraiser</h2><br></br>
+      <Link id="orderbtn" to="/order"><button class="rounded">Place Order</button></Link><br></br>
+      <h2>High quality locally sourced firewood</h2>
+      <h2>at LOW LOW prices</h2><br></br>
+      <p>All profits go towards supporting local and international charities</p><br></br>
     </>
   );
 };
